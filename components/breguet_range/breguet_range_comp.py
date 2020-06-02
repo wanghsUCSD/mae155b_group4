e3f6287b6294a559d399acd86e83bc765028e0ea
cemacken <cemacken@ucsd.edu>
2020-06-02 12:08:24 -0700

    changed variable names

--- components/breguet_range/breguet_range_comp.py
+++ components/breguet_range/breguet_range_comp.py
@@ -10,34 +10,34 @@ from openmdao.api import ExplicitComponent
 class BregRangeCo(ExplicitComponent):
 
     def setup(self):
-        self.add_input('flight_speed')
+        self.add_input('sonic_speed')
         self.add_input('LD')
-        self.add_input('R')
+        self.add_input('rnge')
         self.add_input('isp') #This is a dummy variable for ISP
         self.add_output('w_frac')
 
-        self.declare_partials('w_frac', 'flight_speed')
+        self.declare_partials('w_frac', 'sonic_speed')
         self.declare_partials('w_frac', 'LD')
-        self.declare_partials('w_frac', 'R')
+        self.declare_partials('w_frac', 'rnge')
         self.declare_partials('w_frac', 'isp')
 
     def compute(self, inputs, outputs):
 
-        flight_speed = inputs['flight_speed']
+        sonic_speed = inputs['sonic_speed']
         LD = inputs['LD']
-        R = inputs['R']
+        rnge = inputs['rnge']
         isp = inputs['isp']
 
-        outputs['w_frac'] = np.exp(R / (flight_speed * LD * isp))
+        outputs['w_frac'] = np.exp(rnge / (sonic_speed * LD * isp))
 
     def compute_partials(self, inputs, partials):
-        flight_speed = inputs['flight_speed']
+        sonic_speed = inputs['sonic_speed']
         LD = inputs['LD']
-        R = inputs['R']
+        rnge = inputs['rnge']
         isp = inputs['isp']
 
-        partials['w_frac', 'flight_speed'] = ((-1) * R / (flight_speed**2 * LD * isp)) * np.exp(R / (flight_speed * LD * isp))
-        partials['w_frac', 'LD'] = ((-1) * R / (flight_speed * (LD**2) * isp)) * np.exp(R / (flight_speed * LD * isp))
-        partials['w_frac', 'R'] = (1 / (flight_speed * LD * isp)) * np.exp(R / (flight_speed * LD * isp))
-        partials['w_frac', 'isp'] = ((-1) * R / (flight_speed * LD * isp**2)) * np.exp(R / (flight_speed * LD * isp))    
+        partials['w_frac', 'sonic_speed'] = ((-1) * rnge / (sonic_speed**2 * LD * isp)) * np.exp(rnge / (sonic_speed * LD * isp))
+        partials['w_frac', 'LD'] = ((-1) * rnge / (sonic_speed * (LD**2) * isp)) * np.exp(rnge / (sonic_speed * LD * isp))
+        partials['w_frac', 'rnge'] = (1 / (sonic_speed * LD * isp)) * np.exp(rnge / (sonic_speed * LD * isp))
+        partials['w_frac', 'isp'] = ((-1) * rnge / (sonic_speed * LD * isp**2)) * np.exp(rnge / (sonic_speed * LD * isp))    
 
